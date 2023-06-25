77391c483c72bcec7529927087a5dec8c3a9a726
Prince-Omotayo1 <quadriomotayo2024@gmail.com>
2023-06-25 02:25:55 -0700

    half done

---
+++ add.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * f_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_add(stack_t **head, unsigned int counter)
+{
+	stack_t *k;
+	int len = 0, aux;
+
+	k = *head;
+	while (k)
+	{
+		k = k->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	k = *head;
+	aux = k->n + k->next->n;
+	k->next->n = aux;
+	*head = k->next;
+	free(k);
+}
---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *k;
+	(void)counter;
+
+	k = *head;
+	if (k == NULL)
+		return;
+	while (k)
+	{
+		printf("%d\n", k->n);
+		k = k->next;
+	}
+}
