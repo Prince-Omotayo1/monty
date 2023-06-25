6be1515078f4efae8883937623bbb0487cf32411
Prince-Omotayo1 <quadriomotayo2024@gmail.com>
2023-06-25 04:10:02 -0700

    half done

---
+++ swap.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * f_swap - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_swap(stack_t **head, unsigned int counter)
+{
+	stack_t *k;
+	int len = 0, aux;
+
+	k = *head;
+	while (k)
+	{
+		k = k->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	k = *head;
+	aux = k->n;
+	k->n = k->next->n;
+	k->next->n = aux;
+}
